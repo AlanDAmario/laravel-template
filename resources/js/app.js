9b0b14bc5d82f0bd7505478ac8d02b5df2f22cbc
AlanDAmario <vega93@live.it>
2024-07-06 15:49:06 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import "./bootstrap";
+
+// import  default scss styles
+import "~resources/scss/app.scss";
+
+// path for managing img elements
+import.meta.glob(["../img/**"]);
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laravel Template</title>
+        @vite('resources/js/app.js')
+
+    </head>
+    <body>
+        
+    </body>
+    </html>
